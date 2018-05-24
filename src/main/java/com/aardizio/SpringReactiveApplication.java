6bcc1e8e49216263cdd594934c0af5de97dd7560
ardizioa <a.ardizio@emminformatica.it>
2018-05-24 13:00:23 +0200

    REST api doc + spring hateos + spring data rest

--- src/main/java/com/aardizio/SpringReactiveApplication.java
+++ src/main/java/com/aardizio/SpringReactiveApplication.java
@@ -3,7 +3,10 @@ package com.aardizio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringReactiveApplication {
 
 	public static void main(String[] args) {
